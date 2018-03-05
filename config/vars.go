c085838956cbe911cbb86618d7b55b43e8d89b61
Mostafa Alaa <Mostafa.alaa.494@gmail.com>
2018-03-05 18:20:06 +0200

    Added Config vars and removed the drive functionality

--- config/vars.go
+++ config/vars.go
@@ -5,7 +5,7 @@ var (
 	// Submit
 	SubmitName         = "ACML"
 	AdminPassword      = ""
-	// TestPassword       = ""
+	TestPassword       = ""
 	SubmissionDeadline = "1989-03-21T00:00:00+02:00"
 	TeamNameFormat     = "Team %2v"
 	FeaturesEnabled    = map[string]bool{}
@@ -14,14 +14,23 @@ var (
 	GoogleAPIClientSecret      = ""
 	GoogleAPIClientToken       = ""
 	StudentsSheetID            = ""
-	SubmissionsItems           = []map[string]string{}
-	SubmissionsFolderID        = ""
-	SubmissionsMetaDescription = "Uploaded By:\n- {{.FullName}}\n- {{.Email}}\n- {{.ID}}"
+	// SubmissionsItems           = []map[string]string{}
+	// SubmissionsFolderID        = ""
+	// SubmissionsMetaDescription = "Uploaded By:\n- {{.FullName}}\n- {{.Email}}\n- {{.ID}}"
 	EvaluationsCellRange       = "'Evaluations'!B%d"
 	EvaluationsCalendarID      = "primary"
 	EvaluationsWeekStart       = "1989-03-21T00:00:00+02:00"
 	EvaluationsWeekEnd         = "1989-03-21T00:00:00+02:00"
-	// ReservationDaysAhead       = "7"
+	ReservationDaysAhead       = "7"
+	BscWeeklyMinutes		   = "240"
+
+	VRStudetsSheetID           = ""
+	VRCalendarID			   = ""
+	BscVRWeeklyMinutes		   = "360"
+
+	HoloStudentsSheetID		   = ""
+	HoloCalendarID			   = ""
+	BscHoloWeeklyMinutes	   = ""
 
 	// Slack
 	SlackTestToken    = ""
